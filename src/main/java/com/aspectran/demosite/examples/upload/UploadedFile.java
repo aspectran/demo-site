4b61b61ba8e49f3efdbde29bd8a9c766e2f8dee8
Juho Jeong <aspectran@gmail.com>
2023-12-24 19:47:03 +0900

    Added new Aspectran Utilities module, which replaces the existing com.aspectran.core.util package.

--- src/main/java/com/aspectran/demosite/examples/upload/UploadedFile.java
+++ src/main/java/com/aspectran/demosite/examples/upload/UploadedFile.java
@@ -1,8 +1,8 @@
 package com.aspectran.demosite.examples.upload;
 
-import com.aspectran.core.component.bean.annotation.NonSerializable;
-import com.aspectran.core.util.ToStringBuilder;
-import com.aspectran.core.util.json.JsonWriter;
+import com.aspectran.utils.annotation.NonSerializable;
+import com.aspectran.utils.ToStringBuilder;
+import com.aspectran.utils.json.JsonWriter;
 
 import java.io.IOException;
 import java.util.UUID;
